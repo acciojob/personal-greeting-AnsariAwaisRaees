17c684b2b4d6cad5b13867308bfbed6e5d74cc9f
DELL <ansariawaisraees@gamil.com>
2024-03-08 22:13:24 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,13 @@ const App = () => {
   const [name, setName] = useState("");
   return (
     <div>
-        {/* Do not remove the main div */}
         <p>Enter your name:</p>
-        <input onChange={(e) => setName(e.target.value)}></input>
-        <p>{name}</p>
+        <input type="text" onChange={(e) => setName(e.target.value)}/>
+        {
+          name && (
+            <p>Hello {name}!</p>
+          )
+        }
     </div>
   )
 }
