54fc309180177ef00215e29b57b222115bfbd035
DELL <ansariawaisraees@gamil.com>
2024-03-22 16:12:05 +0530

    Solution

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,22 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName] = useState('');
 
-  const [name, setName] = useState("");
+  const handleChange = (event) => {
+    setName(event.target.value);
+  };
+
   return (
     <div>
-        <label htmlFor="name">Enter your name:</label>
-        <input type="text" id="name" onChange={e => setName(e.target.value)}/>
-        <p>{name && name.length>0?"Hello"+name+"!":""}</p>
+      <label htmlFor="nameInput">Enter your name: </label>
+      <input
+        id="nameInput"
+        type="text"
+        value={name}
+        onChange={handleChange} 
+      />
+      {name && <h2>Hello, {name}!</h2>}
     </div>
   );
 }
